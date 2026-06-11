7dfbbd11df558b9c76f5efdc52db5b480d49fba0
Aaron Lopez <aaronlopez.fr@gmail.com>
2026-06-11 09:22:22 -0500

    Footer con hora

--- footer.php
+++ footer.php
@@ -9,10 +9,12 @@
 // date() devuelve la fecha actual del servidor en el formato que le indiques
 // 'd/m/Y' = día/mes/año (ej: 27/05/2026)
 $fechaHoy = date('d/m/Y');
+$horaHoy = date('H:i:s'); // 'H:i:s' = hora:minutos:segundos (ej: 14:30:45)
 ?>
 
 <footer>
     <p>Desarrollo Web VII &mdash; Universidad Tecnológica de Panamá</p>
     <p>Fecha de ejecución: <?= $fechaHoy ?></p>
+    <p>Hora de ejecución: <?= $horaHoy ?></p>
     <p>Aaron López <br>Kevyn Reyes</p>
 </footer>
